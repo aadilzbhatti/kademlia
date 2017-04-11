37a7d7d79b60c31e37c6179bb6a7e4b5442b91aa
prevosis <aadilzbhatti@gmail.com>
2017-04-11 03:06:17 -0500

    fizing

--- dht.go
+++ dht.go
@@ -25,7 +25,6 @@ func createDHT(id []byte) *DHT {
 
 // register dht
 func (d *DHT) remoteLookup(n *Node, target []byte) []*Node {
-	fmt.Printf("%s:%d\n", n.Address, port)
 	client, err := rpc.Dial("tcp", fmt.Sprintf("%s:%d", n.Address, port))
 	if err != nil {
 		log.Println("Error in remote lookup: ", err)
@@ -48,11 +47,6 @@ func (d *DHT) KClosestRPC(target *[]byte, reply *[]*Node) error {
 
 func (d *DHT) lookup(target []byte) []*Node {
 	kclosest := d.Rt.getKClosest(target).nodes
-	fmt.Println("--KCLOSEST--")
-	for _, v := range kclosest {
-		fmt.Printf("%v, ", v)
-	}
-	fmt.Println("")
 	closest := kclosest[0]
 
 	seen := make(map[string]bool)
@@ -65,10 +59,6 @@ func (d *DHT) lookup(target []byte) []*Node {
 	numresponses := 0
 	i := 0
 	for (numresponses) < ksize && i < (shortlist.Len()) {
-		for _, v := range shortlist.nodes {
-			fmt.Printf("%v, ", v)
-		}
-		fmt.Println("")
 		if seen[string(shortlist.nodes[i].ID)] {
 			i++
 			continue
