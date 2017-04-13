b9905057395d13fd9746508fece4f591d96b5654
prevosis <aadilzbhatti@gmail.com>
2017-04-13 10:48:22 -0500

    fix

--- dht.go
+++ dht.go
@@ -73,7 +73,9 @@ func (d *DHT) lookup(target []byte) []*Node {
 			log.Printf("Node %v has failed\n", shortlist.nodes[i].ID)
 			log.Printf("%v -> shortlist\n", shortlist.nodes)
 			for i, v := range self.Rt.buckets {
+				fmt.Printf("%d, %v\n", i, v)
 				for j, n := range v.bucket {
+					fmt.Printf("%d, %c\n", j, n)
           if n != nil {
             if string(n.ID) == string(shortlist.nodes[i].ID) {
   						self.Rt.buckets[i].bucket = append(self.Rt.buckets[i].bucket[:j], self.Rt.buckets[i].bucket[j + 1:]...) // DELETE NODE FROM BUCKET IF DEAD
